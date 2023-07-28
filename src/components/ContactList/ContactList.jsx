ea7a9cebbca58c31a250ffb09a4d5f274d87f13b
Sergey Kuran <kuran.serg@gmail.com>
2023-07-28 08:50:12 +0300

    Edit files

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,35 +3,37 @@ import React from 'react';
 //  Імпорт стилів
 import css from '../ContactList/ContactList.module.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { deleteContacts, getContacts } from 'redux/contactSlice';
-import { getFilter } from 'redux/filterSlice';
+import { deleteContact } from 'redux/fetch';
+import { getContacts, getFilter } from 'redux/selector';
 // Компонеет для рендеру списку контактів
 
 export const ContactList = () => {
-  const { contacts } = useSelector(getContacts);
-  const selector = useSelector(getFilter);
   const dispatch = useDispatch();
+  const filterValue = useSelector(getFilter);
+  const contacts = useSelector(getContacts);
+
   const getVisible = () =>
-    contacts.filter(e => e.name.toLowerCase().includes(selector.toLowerCase()));
+    contacts.items?.filter(e =>
+      e.name.toLowerCase().includes(filterValue.toLowerCase())
+    );
   const getVisibleContacts = getVisible();
 
   return (
     <div>
       <ul className={css.ul}>
-        {selector &&
-          getVisibleContacts.map(({ id, name, number }) => (
-            <li key={id}>
-              {name}: {number}
-              <button
-                type="button"
-                onClick={() => {
-                  dispatch(deleteContacts(id));
-                }}
-              >
-                Delete
-              </button>
-            </li>
-          ))}
+        {getVisibleContacts?.map(({ id, name, number }) => (
+          <li key={id}>
+            {name}: {number}
+            <button
+              type="button"
+              onClick={() => {
+                dispatch(deleteContact(id));
+              }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
