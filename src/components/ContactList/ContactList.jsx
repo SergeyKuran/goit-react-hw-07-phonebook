b072198bfab1fba608810bc39ad7ecbfbe2c5972
Sergey Kuran <kuran.serg@gmail.com>
2023-07-29 14:08:52 +0300

    Update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from 'redux/fetch';
+import { getFilter } from 'redux/filterSlice';
 import { getContacts } from 'redux/selector';
 
 import css from '../ContactList/ContactList.module.css';
@@ -8,11 +9,16 @@ import css from '../ContactList/ContactList.module.css';
 export const ContactList = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(getContacts);
+  const selector = useSelector(getFilter);
+
+  const getVisible = () =>
+    contacts.filter(e => e.name.toLowerCase().includes(selector.toLowerCase()));
+  const getVisibleContacts = getVisible();
 
   return (
     <div>
       <ul className={css.ul}>
-        {contacts.map(({ id, name, phone }) => (
+        {getVisibleContacts.map(({ id, name, phone }) => (
           <li key={id}>
             {name}: {phone}
             <button
